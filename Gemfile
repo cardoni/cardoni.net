b26ab3e982e540414f1feeb51c572e61a87e230b
Greg Cardoni <greg@cardoni.net>
2013-10-04 18:31:08 -0700

    cleanup gemfile; tweak config

--- Gemfile
+++ Gemfile
@@ -1,9 +1,5 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
-gem "middleman", "~> 3.1.5"
-gem "middleman-blog", "~> 3.3.0"
-
-# For feed.xml.builder
-gem "builder", "~> 3.0"
+gem "middleman",          "~> 3.1.5"
+gem "middleman-blog",     "~> 3.3.0"
+gem "builder",            "~> 3.0"      # feed.xml.builder
--- config.rb
+++ config.rb
@@ -27,7 +27,7 @@ end
 
 page "/feed.xml", :layout => false
 
-### 
+###
 # Compass
 ###
 
@@ -45,13 +45,13 @@ page "/feed.xml", :layout => false
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -86,21 +86,21 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
-  
+
   # Minify Javascript on build
   # activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
